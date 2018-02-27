490bbb416bb5311b5399b992e364261011e1fc48
maxesse <max@maxsanna.com>
2018-02-27 16:38:12 +0000

    Added model and basic UI Interface

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -7,19 +7,20 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
     }
-
-
+    
 }
 
